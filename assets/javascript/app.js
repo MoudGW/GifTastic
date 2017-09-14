ba634929ecdb9438511c8270d5a90a07c22a9fed
mohamed mahmoud jedeine <moud@mohameds-MacBook-Air.local>
2017-09-13 23:17:05 -0400

    last commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
     // declarate Globale variables  
-	var topics=['John snow', 'Arya', 'Dog', 'Little finger', 'Dragons', 'Cat', 'Hodor', 'Stark', 'khaleesi','King of north','Cersei'];
+	var topics=['john snow', 'arya', 'Dog', 'little finger', 'dragons', 'Cat', 'hodor', 'stark', 'khaleesi','king of north'];
 	var selected;
 	var gifsrc=[];
 	var gifsrc_st=[];
@@ -21,22 +21,22 @@
 				$('#left').text('');
 				displaygiphy();
 			}else{
-				$('#left').text('');
+				$(document).off();
+				$('#left').empty();
 				selected=$(this).text();
+				$(this).css("opacity", "0.6");
 				displaygiphy();
-				$(this).css("opacity","0.6");
 			}
-
-		});
-		// add hover effect on buttons to help the user to remember the buttons already clicked
- 		$(".buttons").hover(function(){
+        $(".buttons").hover(function(){
         $(this).css("background-color", "white");
         $(this).css("color", "#0074D9");
         }, function(){
         $(this).css("background-color", "#0074D9");
         $(this).css("color", "white");
-
     });
+		});
+		// add hover effect on buttons to help the user to remember the buttons already clicked
+
 	});
 	// declarate function display buttons
 	function displaybuttons()
@@ -57,8 +57,8 @@
 			{
 				// we will display gif as images and get the still and animated images Url stocked in two different Arrays 
 				$('#left').append("<div class='giphy'><p>Rating :"+data.data[i].rating+"</p><img class='images' id='"+i+"' src='"+data.data[i].images.original_still.url+"'/></div>");
-			    gifsrc.push(data.data[i].images.original.url);
-			    gifsrc_st.push(data.data[i].images.original_still.url);
+			    gifsrc[i]=data.data[i].images.original.url;
+			    gifsrc_st[i]=data.data[i].images.original_still.url;
 			}
 		});
 		// add a click listner on .images and triggred function pausePlay 
@@ -69,12 +69,12 @@
         var src=$(this).attr('src');
         var x=parseInt($(this).attr('id'));
         // check if it is a gif, if so give it the src of an img
-        if(gifsrc.indexOf(src)>0)
+        if(gifsrc.indexOf(src)>=0)
 		{
 	    $(this).attr('src',gifsrc_st[x]);	
 	    }
 	    // check if it is a img, if so give it the src of an gif
-	    if(gifsrc_st.indexOf(src)>0)
+	    if(gifsrc_st.indexOf(src)>=0)
 		{
 	    $(this).attr('src',gifsrc[x]);	
 	    }
